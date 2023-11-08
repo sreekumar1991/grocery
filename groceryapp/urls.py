3353d2efe66d2fba99d43e3446e746e959062089
sreekumar1991 <sreekumarpksreezz@gmail.com>
2023-11-08 10:56:21 +0530

    added

--- groceryapp/urls.py
+++ groceryapp/urls.py
@@ -3,5 +3,15 @@ from. import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('shop/', views.shop, name='shop'),
+    path('about/', views.about, name='about'),
+    path('cart/', views.cart, name='cart'),
+    path('checkout/', views.checkout, name='checkout'),
+    path('contactus/', views.contactus, name='contactus'),
+    path('gallery/', views.gallery, name='gallery'),
+    path('myaccount/', views.myaccount, name='myaccount'),
+    path('shopdetails/', views.shopdetails, name='shopdetails'),
+    path('wishlist/', views.wishlist, name='wishlist'),
+
 
 ]
