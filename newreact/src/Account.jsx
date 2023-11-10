2726902d0f4026c4c37fd57f775faf544e275a53
LIJU321 <Lijujose4505@gmail.com>
2023-11-10 07:20:11 +0530

    some layout changes

--- newreact/src/Account.jsx
+++ newreact/src/Account.jsx
@@ -17,7 +17,7 @@ import Payment from "./credit-card.png";
 import Paypal from "./paypal.png";
 import Amazone from "./amazon-pay.png";
 import gold from "./jewelry1.png";
-import craft from "./tailoring.png";
+import craft from "./adv.png";
 import Boutique from "./boutique.png";
 import gift from "./presents.png";
 import acc from "./account2.png";
@@ -31,44 +31,48 @@ export default function Account() {
   };
 
   const handlelogout = () => {
-    console.log("HIII");
+
     setpopstate(true);
     console.log(popupstate);
   };
 
   const handleclose = () => {
+    alert("HIII");
     setpopstate(false);
   };
 
-
-
+  const click = () => {
+    alert("clicked");
+  };
 
   return (
     <div id="Outside_mainddiv">
       <div id="Sidebar">
-        <Typography id="options">LOGO</Typography> 
+        <div className="optioncontainer" onClick={click}>
+          <Typography id="options">Home</Typography>
+        </div>
         <div id="alloptionconatainer">
-          <div id="optioncontainer">
+          <div className="optioncontainer">
             <img id="optionicons" src={order} alt="order"></img>
             <Typography id="options2">your Orders</Typography>
           </div>
-          <div id="optioncontainer">
+          <div className="optioncontainer">
             <img id="optionicons" src={Security} alt="order"></img>
             <Typography id="options2">Login & security</Typography>
           </div>
-          <div id="optioncontainer">
+          <div className="optioncontainer">
             <img id="optionicons" src={Address} alt="order"></img>
             <Typography id="options2">Your Addresses</Typography>
           </div>
-          <div id="optioncontainer">
+          <div className="optioncontainer">
             <img id="optionicons" src={Payment} alt="order"></img>
             <Typography id="options2">Payment options</Typography>
           </div>
-          <div id="optioncontainer">
+          <div className="optioncontainer">
             <img id="optionicons" src={Paypal} alt="order"></img>
             <Typography id="options2">PayPal</Typography>
           </div>
-          <div id="optioncontainer">
+          <div className="optioncontainer">
             <img id="optionicons" src={Amazone} alt="order"></img>
             <Typography id="options2">Amazon Pay balance</Typography>
           </div>
@@ -88,58 +92,71 @@ export default function Account() {
             ></img>
           </div>
         </div>
-        {/* <div id="test"></div> */}
 
         <div id="menu">
           <div id="allmenucontainer">
             <div id="menuoptions">
               <img src={gold} id="optionicon2" alt="img"></img>
-              <Typography id="description">Gold & Diamond Jewellery</Typography>
-              <Typography id="description2">
-                Apps and more Content and devices Music settings
-              </Typography>
+              <div className="descript1">
+                <Typography id="description">
+                  Gold & Diamond Jewellery
+                </Typography>
+                <Typography id="description2">
+                  Apps and more Content and devices Music settings
+                </Typography>
+              </div>
             </div>
             <div id="menuoptions">
               <img id="optionicon2" src={craft} alt="img"></img>
-              <Typography id="description">
-                Handloom & Handicraft Store
-              </Typography>
-              <Typography id="description2">
-                Advertising preferences Communication preferences SMS alert
-                preferences Message center
-              </Typography>
+              <div className="descript1">
+                <Typography id="description">
+                  Handloom & Handicraft Store
+                </Typography>
+                <Typography id="description2">
+                  Advertising preferences Communication preferences SMS alert
+                  preferences Message center
+                </Typography>
+              </div>
             </div>
             <div id="menuoptions">
               <img id="optionicon2" src={Boutique} alt="img"></img>
-              <Typography id="description">The Designer Boutique</Typography>
-              <Typography id="description2">
-                Amazon Pay Bank accounts for refunds Coupons
-              </Typography>
+              <div className="descript1">
+                <Typography id="description">The Designer Boutique</Typography>
+                <Typography id="description2">
+                  Amazon Pay Bank accounts for refunds Coupons
+                </Typography>
+              </div>
             </div>
             <div id="menuoptions">
               <img id="optionicon2" src={gift} alt="img"></img>
-              <Typography id="description">
-                Gift Boxes, Gift Tags, Greeting Cards
-              </Typography>
-              <Typography id="description2">
-                Apps and more Content and devices Music settings
-              </Typography>
+              <div className="descript1">
+                <Typography id="description">
+                  Gift Boxes, Gift Tags, Greeting Cards
+                </Typography>
+                <Typography id="description2">
+                  Apps and more Content and devices Music settings
+                </Typography>
+              </div>
             </div>
             <div id="menuoptions">
               <img id="optionicon2" src={acc} alt="img"></img>
-              <Typography id="description">Other accounts</Typography>
-              <Typography id="description2">
-                Apps and more Content and devices Music settings
-              </Typography>
+              <div className="descript1">
+                <Typography id="description">Other accounts</Typography>
+                <Typography id="description2">
+                  Apps and more Content and devices Music settings
+                </Typography>
+              </div>
             </div>
             <div id="menuoptions">
               <img id="optionicon2" src={shop} alt="order"></img>
-              <Typography id="description">
-                Shopping programs and rentals
-              </Typography>
-              <Typography id="description2">
-                Apps and more Content and devices Music settings
-              </Typography>
+              <div className="descript1">
+                <Typography id="description">
+                  Shopping programs and rentals
+                </Typography>
+                <Typography id="description2">
+                  Apps and more Content and devices Music settings
+                </Typography>
+              </div>
             </div>
           </div>
         </div>
