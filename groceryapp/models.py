64d695671d233d204b8a42bb900f38d8e2cf3753
LIJU321 <Lijujose4505@gmail.com>
2023-12-31 10:27:54 +0530

    create read operations successful in sql dbms

--- groceryapp/models.py
+++ groceryapp/models.py
@@ -8,8 +8,7 @@ class userData(models.Model):
     Email = models.CharField(max_length=100)
     PanCard = models.CharField(max_length=10)
     AdhaarCard = models.IntegerField(default=0)
-    Password = models.CharField(max_length=100)
-    Confirm_Password = models.CharField(max_length=100)
+
 
 
 
